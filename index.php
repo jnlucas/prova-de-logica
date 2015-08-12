f5b0e0999fcfdb0b248edc70d4043a6211f939d5
joao lucas <lucas.desenv.web@gmail.com>
2015-08-12 16:09:02 -0300

    prova de logica

--- index.php
+++ index.php
@@ -4,11 +4,15 @@ include_once 'entity/Pergaminho.php';
 include_once 'controller/ExercicioController.php';
 
 $exercicio = new ExercicioController();
-$texto = $exercicio->ordenacao();
+
 
 echo "Exercicio 1: ".sizeof($exercicio->getGeneriques());
+echo "<br>";
+
 echo "Exercicio 2: "."verbos:" .sizeof($exercicio->getVerbos());
+echo "<br>";
 
 echo "Exercicio 2: "."verbos Primeira pessoa:" .sizeof($exercicio->getVerbosPrimeiraPessoa($exercicio->getVerbos()));
+echo "<br>";
 
 ?>
